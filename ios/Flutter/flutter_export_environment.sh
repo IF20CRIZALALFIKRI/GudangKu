4a2dad764c09f9aa3f253bd08a5a81ca394d2530
rizalhimself <rizalhimself@gmail.com>
2022-06-29 20:18:03 +0700

    initial

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/home/rizalhimself/.flutter/flutter"
+export "FLUTTER_APPLICATION_PATH=/mnt/9E5486B654869127/rizalhimself/Documents/Pemrograman/SMT6/PMO_GudangKu"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
