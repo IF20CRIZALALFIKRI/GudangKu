e3d7b408efa3de512be2125de57672793344a961
rizalhimself <rizalhimself@gmail.com>
2022-06-30 13:25:02 +0700

    Menghubungkan login dengan dashboard

--- lib/Screen/login.dart
+++ lib/Screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gudangku/Screen/Dashboard.dart';
 import 'package:gudangku/Screen/signup.dart';
 
 class LoginPage extends StatelessWidget {
@@ -30,94 +31,99 @@ class LoginPage extends StatelessWidget {
           children: <Widget>[
             Expanded(
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Text(
-                          "Login",
-                          style:
+                    Text(
+                      "Login",
+                      style:
                           TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                        ),
-                        SizedBox(
-                          height: 20,
-                        ),
-                        Text(
-                          "Login to your account",
-                          style: TextStyle(fontSize: 15, color: Colors.grey[700]),
-                        )
-                      ],
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Column(
-                        children: <Widget>[
-                          inputFile(label: "Email"),
-                          inputFile(label: "Password", obscureText: true)
-                        ],
-                      ),
+                    SizedBox(
+                      height: 20,
                     ),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 40),
-                      child: Container(
-                        padding: EdgeInsets.only(top: 3, left: 3),
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(50),
-                            border: Border(
-                              bottom: BorderSide(color: Colors.black),
-                              top: BorderSide(color: Colors.black),
-                              left: BorderSide(color: Colors.black),
-                              right: BorderSide(color: Colors.black),
-                            )),
-                        child: MaterialButton(
-                          minWidth: double.infinity,
-                          height: 60,
-                          onPressed: () {},
-                          color: Color(0xff0095FF),
-                          elevation: 0,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
-                          ),
-                          child: Text(
-                            "Login",
-                            style: TextStyle(
-                              fontWeight: FontWeight.w600,
-                              fontSize: 18,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text("Don't have an account?"),
-                        TextButton(
-                          child: Text(
-                            "Signup",
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => SignupPage()));
-                          },
-                        )
-                      ],
-                    ),
-                    Container(
-                      padding: EdgeInsets.only(top: 100),
-                      height: 200,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: AssetImage("assets/background.png"),
-                            fit: BoxFit.fitHeight),
-                      ),
+                    Text(
+                      "Login to your account",
+                      style: TextStyle(fontSize: 15, color: Colors.grey[700]),
                     )
                   ],
-                ))
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Column(
+                    children: <Widget>[
+                      inputFile(label: "Email"),
+                      inputFile(label: "Password", obscureText: true)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 40),
+                  child: Container(
+                    padding: EdgeInsets.only(top: 3, left: 3),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        border: Border(
+                          bottom: BorderSide(color: Colors.black),
+                          top: BorderSide(color: Colors.black),
+                          left: BorderSide(color: Colors.black),
+                          right: BorderSide(color: Colors.black),
+                        )),
+                    child: MaterialButton(
+                      minWidth: double.infinity,
+                      height: 60,
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const Dashboard()));
+                      },
+                      color: Color(0xff0095FF),
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(50),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("Don't have an account?"),
+                    TextButton(
+                      child: Text(
+                        "Signup",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignupPage()));
+                      },
+                    )
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.only(top: 100),
+                  height: 200,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/background.png"),
+                        fit: BoxFit.fitHeight),
+                  ),
+                )
+              ],
+            ))
           ],
         ),
       ),
@@ -146,7 +152,7 @@ Widget inputFile({label, obscureText = false}) {
               borderSide: BorderSide(color: Colors.grey),
             ),
             border:
-            OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
+                OutlineInputBorder(borderSide: BorderSide(color: Colors.grey))),
       ),
       SizedBox(
         height: 10,
